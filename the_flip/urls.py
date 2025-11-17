8d4399a40994348b5c1260475eaf052cc0a014c8
Dean Moses <moses@tacocat.com>
2025-11-17 02:09:43 -0700

    Display images

--- the_flip/urls.py
+++ the_flip/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import RedirectView
@@ -58,3 +60,6 @@ path("", RedirectView.as_view(pattern_name="problem-report-list", permanent=Fals
     path("logs/<slug:slug>/", maintenance_views.MachineLogView.as_view(), name="log-machine"),
     # path("logs/new/", maintenance_views.LogEntryCreateView.as_view(), name="log-create"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
