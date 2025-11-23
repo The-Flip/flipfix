fd861155919595cb852937e32763109924b2e294
Dean Moses <moses@tacocat.com>
2025-11-22 23:31:32 -0800

    Stop automatically importing legacy data on every build
    Don't automatically add the old data on deploy

--- build.sh
+++ build.sh
@@ -20,12 +20,3 @@ python manage.py migrate
 
 # Collect static files
 python manage.py collectstatic --no-input
-
-# Import legacy maintainers and make some of them admins
-python manage.py import_maintainers
-
-# Import legacy pinball machines
-python manage.py import_machines
-
-# Import legacy maintenance records
-python manage.py import_maintenance_records
